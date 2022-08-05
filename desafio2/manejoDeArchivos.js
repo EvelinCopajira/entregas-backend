6956ed1482d4d0665740057ef3a87e6cf7f55692
Evelin Copajira <evelincopajira@gmail.com>
2022-08-05 01:56:28 -0300

    Agrego método getById

--- desafio2/manejoDeArchivos.js
+++ desafio2/manejoDeArchivos.js
@@ -35,11 +35,26 @@ class Contenedor {
   }
 
   async getById(id) {
-    return "";
+    //Obtenengo los datos existentes con getAll()
+    const objetos = await this.getAll();
+
+    //.filter para identificar el objeto a traer por id
+    const objetoFiltrado = objetos.filter((elemento) => elemento.id === id);
+    //Si al buscar no encuentra nada, entonces el resultado no existe que traiga error
+    if (objetoFiltrado.length === 0) {
+      console.log(`Error al buscar: no se encontró el id ${id}`);
+    }
+    //Si existe, leo todo el archivo de productos.txt y parseo el string objetos usando async/await - try/catch
+    try {
+      await fs.writeFile(this.ruta, JSON.stringify(objetoFiltrado, null, 2));
+      return JSON.parse(objetos);
+    } catch (error) {
+      return [];
+    }
   }
 
   async getAll() {
-    //Leo todo el archivo de productos.txt y parseo el string productos usando async/await - try/catch
+    //Leo todo el archivo de productos.txt y parseo el string objetos usando async/await - try/catch
     try {
       const objetos = await fs.readFile(this.ruta, "utf-8");
       return JSON.parse(objetos);
@@ -52,29 +67,46 @@ class Contenedor {
     //Obtenengo los datos existentes con getAll()
     const objetos = await this.getAll();
 
-    //.filter para identificar el objeto a elimianr por id
-    const objetosFiltrados = objetos.filter((elemento) => elemento.id != id);
+    //.filter para identificar el objeto a elimianar por id
+    const objetoFiltrado = objetos.filter((elemento) => elemento.id !== id);
     //Si el id no existe no cambió el tamaño de mi []
-    if (objetosFiltrados.length === objetos.length) {
+    if (objetoFiltrado.length === objetos.length) {
       throw new Error(`Error al borrar: no se encontró el ${id}`);
     }
 
-    //Escribir/guardar el nuevo [] con el objeto agregado usando async/await - try/catch
+    //Escribir/guardar el nuevo [] con el objeto borrado usando async/await - try/catch
     try {
-      await fs.writeFile(this.ruta, JSON.stringify(objetosFiltrados, null, 2));
+      await fs.writeFile(this.ruta, JSON.stringify(objetoFiltrado, null, 2));
     } catch (error) {
-      throw new Error(`Error al guardar: ${error}`);
+      throw new Error(`Error: ${error}`);
     }
   }
 
-  async deleteAll() {}
+  async deleteAll() {
+    //Vacío el [] completo y re escribo
+    try {
+      const arrayVacio = [];
+      await fs.writeFile(this.ruta, JSON.stringify(arrayVacio, null, 2));
+    } catch (error) {
+      throw new Error(`Error: ${error}`);
+    }
+  }
 }
 
 //Creo elementos/listas dentro del contenedor con el constructor
 const listaProductos = new Contenedor("./productos.txt");
 
-//Listado de productos - agregar
-//listaProductos.save({ title: "Remera", price: 100 });
+//Listado de productos - agregar SAVE
+//listaProductos.save({ title: "Pantalon corto", price: 150 });
 
-//Listado de productos - borrar por id
-listaProductos.deleteById(1)
+//Listado de productos - buscar por id GET BY ID
+listaProductos.getById(2);
+
+//Listado de productos - traer todos los disponibles GET ALL
+//listaProductos.getAll();
+
+//Listado de productos - borrar por id DELETE BY ID
+//listaProductos.deleteById(1);
+
+//Listado de productos - vaciar todo DELETE ALL
+//listaProductos.deleteAll()
